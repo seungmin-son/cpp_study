99720868ebf777567357b9843eabcac65c227690
seungmin-son <sonsean89@gmail.com>
2023-01-21 16:11:08 +0900

    change3

---
+++ chapter1/1.4.cpp
@@ -0,0 +1,26 @@
+#include <iostream> //cout,cin,endl....
+#include <cstdio> //printf
+
+int main(void)
+{
+    // using namespace std;
+    // int x = 120;
+
+
+    // std::cout << "문장 시작!";
+    // std::cout << "x는" << x << std::endl;
+
+    // std::cout << "abc" << "\t" <<"def"<< std::endl;
+    // std::cout << "ab" << "\t" <<"cdef" << std::endl;
+
+    // cout << "안녕하세요!" << endl;
+    // cout << "\a"; //소리 안들리는데용 ㅎㅎ..
+    
+    using namespace std;
+    int x;
+    cin >> x;
+    cout << "입력값: " << x << endl;
+
+
+    return 0; //return statement 
+}       
