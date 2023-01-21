ada601fa37a06abb4ff67ff4013a570038b6441d
seungmin-son <sonsean89@gmail.com>
2023-01-21 15:49:54 +0900

    ch1.4.ver1

--- ch1.cpp
+++ ch1.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
+#include <iostream> //cout,cin,endl....
+#include <cstdio> //printf
 
 int main(void)
 {
-    int x=1;
-    x = x+2;
-    std::cout << x << std::endl;
+    using namespace std;
+    int x = 120;
 
-    int y = x;
-    std::cout << y << std::endl;
 
-    std::cout << x+ y << std::endl;
-    std::cout << x << std::endl;
+    std::cout << "문장 시작!";
+    std::cout << "x는" << x << std::endl;
 
-    int z;
-    std::cout << z << std::endl;
+    std::cout << "abc" << "\t" <<"def"<< std::endl;
+    std::cout << "ab" << "\t" <<"cdef" << std::endl;
+
+    cout << "안녕하세요!" << endl;
+    cout << "\a";
 
     
     return 0; //return statement 
