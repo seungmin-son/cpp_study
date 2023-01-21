bedb5e1abb064c2e577a1e3f1a9c191fe8cc4653
seungmin-son <sonsean89@gmail.com>
2023-01-21 15:22:58 +0900

    ch1.3_change1

--- ch1.cpp
+++ ch1.cpp
@@ -2,13 +2,19 @@
 
 int main(void)
 {
-    int x = 2;
-    x = 5;
-    int y = x+3;
+    int x=1;
+    x = x+2;
+    std::cout << x << std::endl;
 
+    int y = x;
     std::cout << y << std::endl;
-    std::cout << &y << std::endl;
 
+    std::cout << x+ y << std::endl;
+    std::cout << x << std::endl;
 
+    int z;
+    std::cout << z << std::endl;
+
+    
     return 0; //return statement 
-}
+}   
