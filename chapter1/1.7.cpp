ac181e9b761e332fafdd18939b3e9ae57902f345
seungmin-son <sonsean89@gmail.com>
2023-01-21 17:37:49 +0900

    change5

---
+++ chapter1/1.7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+void doSomething(int x)
+{
+    x = 123;
+    cout << x << endl; //5
+
+}
+
+
+
+
+int main()
+{
+    int x =0;
+    cout << x <<" "<< &x<< endl; //1
+
+    {
+        int x=1;
+        cout << x <<" "<< &x<< endl; //2
+                
+    }
+
+    {
+        int x=2;
+        cout << x <<" "<< &x<< endl; //3
+    }
+   cout << x <<" "<< &x<< endl; //4
+   doSomething(x);
+   cout << x <<" "<< &x<< endl; //6
+
+    return 0;
+}
